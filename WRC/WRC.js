64035b957207dd97103c9626f434f3b1d10324dc
Joseph Mann <jlatmann@gmail.com>
2015-03-10 13:24:45 -0600

    got directions working with autofill !
    need current location integration

--- WRC/WRC.js
+++ WRC/WRC.js
@@ -58,51 +58,108 @@ if (Meteor.isClient) {
     });
   });
 
-
-  Template.jmap.helpers({
-    exampleMapOptions: function() {
-      origin = Geolocation.currentLocation(); // This worked !!! (how??)
-      // Make sure the maps API has loaded
-
-      if (GoogleMaps.loaded()) {
-        if (! origin) {
-          return {
-            center:  new google.maps.LatLng(0.0, 0.0),
-            zoom: 10
-          }
-        }
-        // Map initialization options
-        console.log("in exampleMapOptions", origin);
-        return {
-          center: new google.maps.LatLng(origin.coords.latitude, origin.coords.longitude),
-          zoom: 10
-        };
-      }
+  function __addLatLng(event) {
+    origin = map.center;
+    if (! destination) {
+      destination = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
     }
-  });
+    else {
+      origin = destination;
+      destination = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+    }
+    console.log("map click event!");
+    // Add coordinates into db
+
+    var request = {
+      origin: origin,
+      //waypoints: waypointsArr,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.UnitSystem.METRIC
+    };
+
+    directionsService.route(request, function (response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+        console.log(response);
+        console.log("total time:", response.routes[0].legs[0].duration.text);
+        console.log("total distance:", response.routes[0].legs[0].distance.text);
+        //console.log("total distance:", response.routes[0].legs[0].distance.value / 1000.0);
+      }
+    });
+
+
+    console.log("lat: ", event.latLng.lat());
+    // this is supposed to make a new marker.. ad hoc
+    // taken from above
+    new google.maps.Marker({
+      position: destination,
+      map: map,
+      title: 'Alphonso',
+      draggable: true
+    });
+  }
+
+
 
   Template.kmap.rendered = function() {
     Tracker.autorun(function () {
+      // Tracker.autorun makes the function reactive (like templates)
+      var start;
+      var destination;
+      var directionsService;
+      var directionsDisplay;
+
       if (GoogleMaps.loaded()) {
         $("input#geocomplete").geocomplete({
           map: '.map_canvas',
-          mapOptions: {
-            zoom: 10,
-            center: new google.maps.LatLng(0.0,0.0)
-          }
+          location: 'Alberta'
+          // mapOptions: {
+          //   zoom: 10,
+          //   center: new google.maps.LatLng(0.0,0.0)
+          // }
         })
         .bind("geocode:result", function(event, result){
-            console.log(result);
+          start = result.geometry.location;
+          console.log('start', result);
         });
+
         mmap = $("input#geocomplete").geocomplete("map");
+        directionsService = new google.maps.DirectionsService();
+      //  google.maps.event.addListener(mmap, 'click', addLatLng);
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(mmap);
+
+
         $("input#destination").geocomplete()
         .bind("geocode:result", function(event, result){
-          console.log(result.geometry.location);
-            new google.maps.Marker({
-              position: result.geometry.location,
-              map: mmap // this worked !!
-            });
+          destination = result.geometry.location;
+          console.log(destination);
+          new google.maps.Marker({
+            position: destination, //result.geometry.location,
+            map: mmap // this worked !!
+          });
 
+          // setup request for directions
+          var request = {
+            origin: start,
+            //waypoints: waypointsArr,
+            destination: destination,
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC
+          };
+
+          directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              directionsDisplay.setDirections(response);
+              console.log(response);
+              console.log("total time:", response.routes[0].legs[0].duration.text);
+              console.log("total distance:", response.routes[0].legs[0].distance.text);
+              //console.log("total distance:", response.routes[0].legs[0].distance.value / 1000.0);
+            }
+          });
+
+          //
         });
       }
     });
@@ -119,6 +176,30 @@ if (Meteor.isClient) {
 /// ********** THIS IS UGLY *******************
 // Template.jmap.created is called by meteor as soon as the template instance
 // has been created but before it has been rendered.
+Template.jmap.helpers({
+  exampleMapOptions: function() {
+    origin = Geolocation.currentLocation(); // This worked !!! (how??)
+    // Make sure the maps API has loaded
+
+    if (GoogleMaps.loaded()) {
+      if (! origin) {
+        return {
+          center:  new google.maps.LatLng(0.0, 0.0),
+          zoom: 10
+        }
+      }
+      // Map initialization options
+      console.log("in exampleMapOptions", origin);
+      return {
+        center: new google.maps.LatLng(origin.coords.latitude, origin.coords.longitude),
+        zoom: 10
+      };
+    }
+  }
+});
+
+
+
   Template.jmap.created = function() {
     var  destination;
 
